4fe735d9c4ded956a830e93faf608e811a0c67d3
399 <399@163.com>
2026-04-24 20:18:53 +0800

    refactor: delete unused app config stubs

--- dashboard_api/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DashboardApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dashboard_api'
-    verbose_name = '前后端分离后台 API'
